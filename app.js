7b876f44966c214573ab44af18b2183772be562a
Sergio Andrés Valero Linares <sergiovalero0922@gmail.com>
2025-03-17 13:15:17 -0500

    Crea función para agregar amigos

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 // Array para almacenar los nombres
 
 let amigos = [];
+
+// Función para agregar amigos
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById ('amigo');
+    let nombreAmigo = inputAmigo.value.trim();
+    console.log('nombreAmigo');
+
+    if(nombreAmigo === ""){
+        alert("Por favor, inserte un nombre");
+        return;
+    }
+
+    if (amigos.includes(nombreAmigo)) {
+        alert(`El nombre ${nombreAmigo} ya se encuentra en la lista`);
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    inputAmigo.value ="";
+
+    actualizarLista();
